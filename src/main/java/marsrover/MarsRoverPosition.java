1ce39cd2963be7110dfcf939c57c2de8d3a749d8
beichengguiyan <chihaotian@zybank.com.cn>
2020-07-21 20:18:33 +0800

    refator:remove class Coordinates

--- src/main/java/marsrover/MarsRoverPosition.java
+++ src/main/java/marsrover/MarsRoverPosition.java
@@ -2,28 +2,30 @@ package marsrover;
 
 
 public class MarsRoverPosition {
-    private Coordinates coordinates;
+    private int coordinatesX;
+    private int coordinatesY;
     private Direction direction;
 
     public MarsRoverPosition(int coordinatesX, int coordinatesY, String direction) {
-        this.coordinates = new Coordinates(coordinatesX, coordinatesY);
+        this.coordinatesX = coordinatesX;
+        this.coordinatesY = coordinatesY;
         this.direction = Direction.fromShortName(direction);
     }
 
     public int getCoordinatesX() {
-        return coordinates.getCoordinatesX();
-    }
-
-    public int getCoordinatesY() {
-        return coordinates.getCoordinatesY();
+        return coordinatesX;
     }
 
     public void setCoordinatesX(int coordinatesX) {
-         coordinates.setCoordinatesX(coordinatesX);
+        this.coordinatesX = coordinatesX;
+    }
+
+    public int getCoordinatesY() {
+        return coordinatesY;
     }
 
     public void setCoordinatesY(int coordinatesY) {
-         coordinates.setCoordinatesY(coordinatesY);
+        this.coordinatesY = coordinatesY;
     }
 
     public String getDirectionShortName() {
